5f5b88d66c0563a661e15388a0a82eefa657f1ce
elenasofia98 <elenasofia98@gmail.com>
2022-06-11 15:22:20 +0200

    minor update

--- src/main/java/TLCMain.java
+++ src/main/java/TLCMain.java
@@ -27,31 +27,26 @@ public class TLCMain {
         String hdfsPort = "9000";
 
         Application app = Application.init(new ClusterConf(hdfsIP, hdfsPort, sparkIP, sparkPort));
-        ArrayList<String> usedColumns = new ArrayList<>();
-        //usedColumns.add("tpep_pickup_datetime");
-        //usedColumns.add("passenger_count");
-
+        ArrayList<String> dimensions = new ArrayList<>();
         //query 1
-        usedColumns.add("tpep_dropoff_datetime");
-        usedColumns.add("tip_amount");
-        usedColumns.add("total_amount");
-        usedColumns.add("tolls_amount");
-        usedColumns.add("payment_type");
-
+        dimensions.add("tpep_dropoff_datetime");
+        dimensions.add("tip_amount");
+        dimensions.add("total_amount");
+        dimensions.add("tolls_amount");
+        dimensions.add("payment_type");
         //query 2
-        usedColumns.add("tpep_pickup_datetime");
-        usedColumns.add("PULocationID");
+        dimensions.add("tpep_pickup_datetime");
+        dimensions.add("PULocationID");
 
 
-        //"airport_fee"
-        ArrayList<String> filenames = new ArrayList<>();
-        filenames.add("/input/yellow_tripdata_2021-12.parquet");
-        filenames.add("/input/yellow_tripdata_2022-01.parquet");
-        filenames.add("/input/yellow_tripdata_2022-02.parquet");
+        String[] filenames = new String[3];
+        filenames[0] = "/input/yellow_tripdata_2021-12.parquet";
+        filenames[1] = "/input/yellow_tripdata_2022-01.parquet";
+        filenames[2] = "/input/yellow_tripdata_2022-02.parquet";
 
 
         try {
-            Dataset<TaxiRoute> dataset = app.load(filenames, usedColumns);
+            Dataset<TaxiRoute> dataset = app.load(filenames, dimensions);
             dataset = dataset.cache();
             dataset.show();
 
@@ -59,12 +54,14 @@ public class TLCMain {
             rdd.cache();
             rdd.take(10).forEach(System.out::println);
 
-            long start = System.currentTimeMillis();
-            /*Per ogni mese solare, calcolare la percentuale media dell’importo della mancia rispetto al costo effettivo
+
+            /*
+            * Per ogni mese solare, calcolare la percentuale media dell’importo della mancia rispetto al costo effettivo
             * della corsa. Calcolare il costo effettivo della corsa come differenza tra Total amount e Tolls amount
             * ed includere soltanto i pagamenti effettuati con carta di credito. Nell’output indicare anche il numero
             * totale di corse usate per calcolare il valore medio*/
-            //app.query1(rdd);
+            long start = System.currentTimeMillis();
+            app.query1(rdd);
             long end = System.currentTimeMillis();
             System.out.println("---------->Duration in millis: " + (end - start));
 
--- src/main/java/batch/Application.java
+++ src/main/java/batch/Application.java
@@ -2,6 +2,7 @@ package batch;
 
 import org.apache.spark.SparkConf;
 import org.apache.spark.api.java.JavaPairRDD;
+import org.apache.spark.api.java.function.FilterFunction;
 import org.apache.spark.sql.*;
 import org.apache.spark.api.java.JavaRDD;
 import scala.Tuple2;
@@ -32,9 +33,7 @@ public class Application {
         this.hdfs = "hdfs://"+ip+":"+port;
     }
 
-    protected static final Comparator<String> comparator = new Query2Comparator();
-
-    private Dataset<Row> load_parquet(ArrayList<String> paths) throws Exception {
+    private Dataset<Row> load_parquet(String[] paths) throws Exception {
         Dataset<Row> dataset = null;
         for(String path: paths) {
             Dataset<Row> temp = this.ss.read().parquet(this.hdfs+path);
@@ -55,38 +54,41 @@ public class Application {
     }
 
 
-    public Dataset<TaxiRoute> load(ArrayList<String> pahts, ArrayList<String> usedColumns) throws Exception {
+    public Dataset<TaxiRoute> load(String[] paths, ArrayList<String> dimensions) throws Exception {
 
-        Dataset<Row> dataset = this.load_parquet(pahts);
+        Dataset<Row> dataset = this.load_parquet(paths);
         for(String c: dataset.columns()){
-            //System.out.println(c);
-            if(!usedColumns.contains(c))
+            if(!dimensions.contains(c))
                 dataset = dataset.drop(c);
         }
 
-        //System.out.println("------>total " + dataset.count());
-        //dataset = dataset.filter((FilterFunction<Row>) row -> ! row.anyNull());
-        //System.out.println("------>not null " + dataset.count());
+        System.out.println("------> total " + dataset.count());
+        dataset = dataset.filter((FilterFunction<Row>) row -> ! row.anyNull());
+        System.out.println("------> not null " + dataset.count());
+
 
         dataset = dataset
-                .withColumn("tpep_dropoff_datetime",
-                        date_format(dataset.col("tpep_dropoff_datetime"), "yyyy/MM/dd hh:mm"))
+                .withColumn(
+                        "tpep_dropoff_datetime",
+                        date_format(dataset.col("tpep_dropoff_datetime"), "yyyy/MM/dd hh:mm")
+                )
+                .withColumn(
+                        "tpep_pickup_datetime",
+                        date_format(dataset.col("tpep_pickup_datetime"), "yyyy/MM/dd hh:mm")
+                )
                 .filter(col("tpep_dropoff_datetime").gt(lit("2021/11/31"))
                         .and(col("tpep_dropoff_datetime").lt(lit("2022/03/01"))))
-                .withColumn("tpep_pickup_datetime",
-                        date_format(dataset.col("tpep_pickup_datetime"), "yyyy/MM/dd hh:mm"))
                 .filter(col("tpep_pickup_datetime").gt(lit("2021/11/31"))
                         .and(col("tpep_pickup_datetime").lt(lit("2022/03/01"))));
+        System.out.println("------> having right date " + dataset.count());
 
-        // tip_amount(double)| tolls_amount(double)|total_amount(double) |month (int)
         dataset = dataset
-                .withColumn("payment_type", dataset.col("payment_type").cast("long"))
-                //.withColumn("passenger_count", dataset.col("passenger_count").cast("int"))
-                .withColumn("tip_amount", dataset.col("tip_amount").cast("double"))
-                .withColumn("tolls_amount", dataset.col("tolls_amount").cast("double"))
-                .withColumn("total_amount", dataset.col("total_amount").cast("double"))
-                .withColumn("PULocationID", dataset.col("PULocationID").cast("long"));
-
+                .withColumn("payment_type"  , dataset.col("payment_type"  ).cast("long")  )
+                .withColumn("tip_amount"    , dataset.col("tip_amount"    ).cast("double"))
+                .withColumn("tolls_amount"  , dataset.col("tolls_amount"  ).cast("double"))
+                .withColumn("total_amount"  , dataset.col("total_amount"  ).cast("double"))
+                .withColumn("PULocationID"  , dataset.col("PULocationID"  ).cast("long")  );
+        System.out.println("------> after cast " + dataset.count());
         return dataset.as(Encoders.bean(TaxiRoute.class));
     }
 
@@ -187,7 +189,7 @@ public class Application {
                 ))
                 .mapValues(doubleIntegerTuple -> new Tuple2<Double, Double>(
                         doubleIntegerTuple._2() / doubleIntegerTuple._3(),
-                        Math.sqrt( ((double) (1/doubleIntegerTuple._3()) * doubleIntegerTuple._1()) - Math.pow(doubleIntegerTuple._2() / doubleIntegerTuple._3(), 2))
+                        Math.sqrt(  ((double) 1/doubleIntegerTuple._3()) * doubleIntegerTuple._1() - Math.pow( ((double)doubleIntegerTuple._2() / doubleIntegerTuple._3()), 2))
                         ));
 
         return mean_tip_by_hour;
